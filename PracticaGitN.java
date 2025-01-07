667efe6fa75cbaccade9cae1f3ed2205a86b45fa
El programador ·_· <ElProgramador@code.santi>
2025-01-07 19:49:34 +0100

    añado dinamicidad

--- PracticaGitN.java
+++ PracticaGitN.java
@@ -4,10 +4,15 @@ public class PracticaGitN
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
-        int[][] matriz = new int[10][10];
+        System.out.print("Cuantas filas quieres: ");
+        int filas = in.nextInt();
+        System.out.print("Cuantas columnas quieres: ");
+        int columnas = in.nextInt();
+
+        int[][] matriz = new int[columnas][filas];
         int indice=0;
-        for(int i = 0; i<10;i++){
-            for(int j = 0; j<10; j++){
+        for(int i = 0; i<columnas;i++){
+            for(int j = 0; j<filas; j++){
                 indice ++;
                 matriz[i][j] = indice;
                 System.out. print(" "+matriz[i][j]+ " ");
